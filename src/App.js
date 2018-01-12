373f48c448e731ee0e0f59c42445130a47760c33
Sung Kyungmo <skm280@naver.com>
2018-01-12 14:57:26 +0900

    fetch로 영화 api 요청

--- src/App.js
+++ src/App.js
@@ -1,48 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
-import Movie from './Movie'
+import Movie from './Movie';
 
 class App extends Component {
 
-  state = {
-  }
+  state = {};
 
-  componentWillMount(){
-    console.log("will mount");
-  }
-
-  componentDidMount(){
-    console.log("did mount");
-    setTimeout(() => {
-      this.setState({
-        movies: [
-          {
-            title: "iron man 1",
-            poster: "https://images-na.ssl-images-amazon.com/images/M/MV5BMTczNTI2ODUwOF5BMl5BanBnXkFtZTcwMTU0NTIzMw@@._V1_UY1200_CR90,0,630,1200_AL_.jpg",
-          },
-          {
-            title: "iron man 2",
-            poster: "https://images-na.ssl-images-amazon.com/images/M/MV5BMTM0MDgwNjMyMl5BMl5BanBnXkFtZTcwNTg3NzAzMw@@._V1_UY1200_CR89,0,630,1200_AL_.jpg",
-          },
-          {
-            title: "iron man 3",
-            poster: "https://images-na.ssl-images-amazon.com/images/M/MV5BMTkzMjEzMjY1M15BMl5BanBnXkFtZTcwNTMxOTYyOQ@@._V1_UY1200_CR105,0,630,1200_AL_.jpg",
-          },
-        ]
-      })
-    },2000 )
+  componentDidMount() {
+    fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating')
+    .then(response => response.json())
+    .then(json => console.log(json))
+    .catch(err => console.log(err))
   }
 
   _renderMovies = () => {
-    const movies = this.state.movies.map((movie, index) => {
-      return <Movie title={movie.title} poster={movie.poster} key={index} />
+    const movies = this.state.movies.map(movie => {
+      return <Movie title={movie.title} poster={movie.small_cover_image} key={movie.id} />
     })
     return movies;
   };
 
   render() {
-    console.log("render");
-    return (
+    return(
       <div className="App">
         {this.state.movies ? this._renderMovies() : 'Loading'}
       </div>
