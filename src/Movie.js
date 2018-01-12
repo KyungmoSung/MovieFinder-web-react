35502de76ecea0b7ca15b58c723165609b7400b9
Sung Kyungmo <skm280@naver.com>
2018-01-12 13:50:33 +0900

    Movie.js, css 생성 및 초기화

---
+++ src/Movie.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import './Movie.css';
+
+class Movie extends Component {
+    render() {
+        return (
+            <div>
+                <h1>hello this is movie app!</h1>
+            </div>
+        );
+    }
+}
+
+
+export default Movie;
