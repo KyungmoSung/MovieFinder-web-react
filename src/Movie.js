fe4e1a2fc981864e35bbacc07b69b456dc85acbb
Sung Kyungmo <skm280@naver.com>
2018-01-12 14:06:47 +0900

    MoviePoster class 추가, title, poster 출력

--- src/Movie.js
+++ src/Movie.js
@@ -6,11 +6,19 @@ class Movie extends Component {
     render() {
         return (
             <div>
-                <h1>hello this is movie app!</h1>
+                <MoviePoster poster={this.props.poster}/>
+                <h1>{this.props.title}</h1>
             </div>
         );
     }
 }
 
+class MoviePoster extends Component {
+    render() {
+        return (
+            <img src={this.props.poster}/>
+        );
+    }
+}
 
 export default Movie;
